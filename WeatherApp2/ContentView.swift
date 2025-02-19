79418329f2c2f137e1a288c12ca7ecc01338995d
Douglas Jasper <doug.jasper@trios.com>
2025-02-19 06:37:37 -0500

    Initial Commit

---
+++ WeatherApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp2
+//
+//  Created by Douglas Jasper on 2025-02-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp2/WeatherApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2App.swift
+//  WeatherApp2
+//
+//  Created by Douglas Jasper on 2025-02-19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
